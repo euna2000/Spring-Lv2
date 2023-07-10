aafaebff02c93958cf173593fa5b508da5a01ce1
euna2000 <131853007+euna2000@users.noreply.github.com>
2023-07-10 22:14:44 +0900

    Update ExceptionController.java 주석

--- src/main/java/com/sparta/springlv2/controller/ExceptionController.java
+++ src/main/java/com/sparta/springlv2/controller/ExceptionController.java
@@ -6,9 +6,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+// API 예외 처리를 담당하는 컨트롤러입니다. @RestControllerAdvice 어노테이션을 통해 전역 예외 처리를 정의합니다.
 @RestControllerAdvice
 public class ExceptionController {
 
+    // handleApiRequestException(): ApiRequestException 예외가 발생했을 때 실행되는 메서드입니다. 
+    // 예외 메시지를 담은 ExceptionResponseDto를 생성하고, 예외의 HTTP 상태 코드를 기반으로 응답을 생성하여 반환합니다.
     @ExceptionHandler(ApiRequestException.class)
     public ResponseEntity<String> handleApiRequestException(ApiRequestException e) {
         return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
