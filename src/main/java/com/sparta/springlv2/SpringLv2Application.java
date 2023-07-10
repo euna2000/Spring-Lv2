e1556a84f49bbdee91ec2bca81925831a5ca2941
euna2000 <131853007+euna2000@users.noreply.github.com>
2023-07-10 23:17:15 +0900

    Update SpringLv2Application.java 주석

--- src/main/java/com/sparta/springlv2/SpringLv2Application.java
+++ src/main/java/com/sparta/springlv2/SpringLv2Application.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+//  이 클래스는 실행하는 역할을 합니다.
 @EnableJpaAuditing
 @SpringBootApplication
 public class BlogApplication {
