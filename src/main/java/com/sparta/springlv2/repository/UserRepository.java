a4fc80461ea571c1e4a4a62f17a5a7a92a612dae
euna2000 <131853007+euna2000@users.noreply.github.com>
2023-07-10 23:14:23 +0900

    Update UserRepository.java 주석

--- src/main/java/com/sparta/springlv2/repository/UserRepository.java
+++ src/main/java/com/sparta/springlv2/repository/UserRepository.java
@@ -3,6 +3,7 @@ package com.sparta.springlv2.repository;
 import com.sparta.springlv2.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+// 사용자 엔티티와 관련된 데이터베이스 작업을 수행하기 위한 JpaRepository 인터페이스를 확장합니다.
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByUsername(String username);
 }
