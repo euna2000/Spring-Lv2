5be0bc75ed983db3f6013aeb15cc8da87c210576
euna2000 <131853007+euna2000@users.noreply.github.com>
2023-07-10 23:15:12 +0900

    Update TokenValidationFilter.java

--- src/main/java/com/sparta/springlv2/security/TokenValidationFilter.java
+++ src/main/java/com/sparta/springlv2/security/TokenValidationFilter.java
@@ -11,6 +11,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+// WT 토큰의 유효성을 검사하는 필터입니다.
+//  JWT 토큰이 올바르지 않거나 만료되었을 경우 인증 정보를 설정하지 않고 요청을 통과시킵니다. 
+// UserService 클래스는 회원 가입과 로그인과 관련된 비즈니스 로직을 제공합니다. 
+// 비밀번호 암호화, 사용자 인증, JWT 토큰 생성 등의 기능을 수행합니다.
 public class TokenValidationFilter extends OncePerRequestFilter {
 
     @Autowired
