6dcc39864b9bcedeef108c9754f3a549ea8375e3
euna2000 <131853007+euna2000@users.noreply.github.com>
2023-07-10 23:07:58 +0900

    Update PostController.java 주석

--- src/main/java/com/sparta/springlv2/controller/PostController.java
+++ src/main/java/com/sparta/springlv2/controller/PostController.java
@@ -11,18 +11,21 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+// 게시글 관련 API를 처리하는 역할을 합니다. 
 @RestController
 @RequiredArgsConstructor
 public class PostController {
 
     private final PostService postService;
 
+    //  "/posts" 경로로 GET 요청이 오면 getAllPosts() 메서드가 실행되고, POST 요청이 오면 createPost() 메서드가 실행됩니다. 
     @GetMapping("/posts")
     public ResponseEntity<List<PostResponseDto>> getAllPosts() {
         List<PostResponseDto> posts = postService.getAllPosts();
         return ResponseEntity.ok(posts);
     }
 
+    // createPost() 메서드는 요청 본문의 PostRequestDto를 받아와 게시글을 생성하고, JWT 토큰에서 사용자 정보를 추출하여 작성자로 설정합니다.
     @PostMapping("/posts")
     public ResponseEntity<PostResponseDto> createPost(@Validated @RequestBody PostRequestDto requestDto, @RequestHeader("Authorization") String token) {
         String username = parseToken(token);
@@ -30,6 +33,7 @@ public class PostController {
         return ResponseEntity.ok(createdPost);
     }
 
+    //  "/posts/{postId}" 경로로 GET 요청이 오면 getPostById() 메서드가 실행되고, PUT 요청이 오면 updatePost() 메서드가 실행되며, DELETE 요청이 오면 deletePost() 메서드가 실행됩니다.
     @GetMapping("/posts/{postId}")
     public ResponseEntity<PostResponseDto> getPostById(@PathVariable Long postId) {
         PostResponseDto post = postService.getPostById(postId);
