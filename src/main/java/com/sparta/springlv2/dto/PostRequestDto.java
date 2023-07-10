229416801e2f27df191532266b14c4eef703f7a9
euna2000 <131853007+euna2000@users.noreply.github.com>
2023-07-10 22:16:54 +0900

    Update PostRequestDto.java 주석

--- src/main/java/com/sparta/springlv2/dto/PostRequestDto.java
+++ src/main/java/com/sparta/springlv2/dto/PostRequestDto.java
@@ -3,6 +3,7 @@ package com.sparta.springlv2.dto;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+// 게시글 작성 및 수정에 사용되는 요청 DTO 클래스입니다. title과 content 필드를 가지고 있습니다.
 @Getter
 @NoArgsConstructor
 public class PostRequestDto {
