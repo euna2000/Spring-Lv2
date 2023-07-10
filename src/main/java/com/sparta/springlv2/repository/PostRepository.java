01686618a250289caf99b4a9d9014ec5774b665a
euna2000 <131853007+euna2000@users.noreply.github.com>
2023-07-10 23:14:00 +0900

    Update PostRepository.java 주석

--- src/main/java/com/sparta/springlv2/repository/PostRepository.java
+++ src/main/java/com/sparta/springlv2/repository/PostRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
+// 게시글 작업을 수행하기 위한 JpaRepository 인터페이스를 확장합니다.
 public interface PostRepository extends JpaRepository<Post, Long> {
     List<Post> findAllByOrderByCreatedAtDesc();
 }
