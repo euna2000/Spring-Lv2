c5045ae81d47f39716d8d1db2c5d7a90d8dd5d66
euna2000 <131853007+euna2000@users.noreply.github.com>
2023-07-10 23:09:08 +0900

    Update UserController.java 주석

--- src/main/java/com/sparta/springlv2/controller/UserController.java
+++ src/main/java/com/sparta/springlv2/controller/UserController.java
@@ -8,12 +8,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+// 회원 가입 및 로그인과 관련된 API를 처리하는 역할을 합니다.
 @RestController
 @RequiredArgsConstructor
 public class UserController {
 
     private final UserService userService;
 
+    //  "/signup" 경로로 POST 요청이 오면 signup() 메서드가 실행되어 회원 가입을 처리하고, "/login" 경로로 POST 요청이 오면 login() 메서드가 실행되어 로그인을 처리합니다.
     @PostMapping("/signup")
     public ResponseEntity<String> signup(@Validated @RequestBody SignupRequestDto requestDto) {
         userService.signup(requestDto);
